6e1d41fc3715f516c35444e5c37b018221cceebb
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-08-15 15:25:15 +0300

    Initial Commit

---
+++ UserApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserApp
+//
+//  Created by ceksi on 15.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserApp/UserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAppApp.swift
+//  UserApp
+//
+//  Created by ceksi on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct UserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
